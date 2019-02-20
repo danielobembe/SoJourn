0f5f905db2381e07a3565a857af4c07757a606ca
Daniel Obembe <obembe.dan@gmail.com>
2019-02-19 19:58:03 -0600

    Adding fake post data

--- sojourn.py
+++ sojourn.py
@@ -2,13 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'',
+        'title':'',
+        'content':'',
+        'date_posted':''
+    },
+    {
+        'author':'',
+        'title':'',
+        'content':'',
+        'date_posted':''
+    },
+    {
+        'author':'',
+        'title':'',
+        'content':'',
+        'date_posted':''
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
