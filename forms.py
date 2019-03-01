cd4d4b33b733250edc5c597af44b32291b28aed0
Daniel Obembe <obembe.dan@gmail.com>
2019-02-28 19:22:08 -0600

    Created register and login forms, register and login templates

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -12,3 +12,10 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', 
             validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+            validators = [DataRequired(), Email()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- sojourn.py
+++ sojourn.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, url_for 
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a20943a0515d374b3280ca50627ed5da'
 
 posts = [
     {
@@ -24,6 +26,16 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
